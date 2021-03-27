28b170b9c3cf08f35a2535c681d1087828a6e14f
Steven Yepes <jstevenyepes@gmail.com>
2021-03-26 19:19:57 -0500

    Initial project of pet clinic

---
+++ src/main/java/com/jsteven/jstvpetclinic/JstvPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jsteven.jstvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstvPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsteven/jstvpetclinic/JstvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsteven.jstvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstvPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
